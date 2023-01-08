3f02208a946a558bcb880284218e5b75561a0b31
Nick Garcia <nicholasgarcia2003@protonmail.com>
2023-01-08 16:51:34 -0500

    backup commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include("templates/header.php"); ?>
+
+<div class="note">
+    <div class="sticky">
+        <input class="stickyTitle" type="text" value="note title"></input>
+        <textarea>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</textarea>
+    </div>
+    <div class="pin"></div>
+</div>
+
+<?php include("templates/footer.php"); ?>
+</html>
---
+++ templates/footer.php
@@ -0,0 +1,2 @@
+<footer>Made by Nick Garcia, 2022.</footer>
+</body>
